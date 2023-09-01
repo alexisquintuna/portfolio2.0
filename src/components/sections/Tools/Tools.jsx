eb9369300017391ecdfe8292084a07aa5b096a0d
Alexis Quintuna <alexisiquintuna@gmail.com>
2023-09-01 15:35:17 -0400

    added a new haunter gif

--- src/components/sections/Tools/Tools.jsx
+++ src/components/sections/Tools/Tools.jsx
@@ -16,7 +16,7 @@ import VerticalLine from "../Line/VerticalLine";
 const data = [
   {
     id: 0,
-    photo: "https://thumbs.gfycat.com/SlimSolidJapanesebeetle-max-1mb.gif",
+    photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dce29357-31f7-4a01-a737-23e473625162/d49462h-cad148b2-cc15-41dc-88bd-a0121705ae57.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZTI5MzU3LTMxZjctNGEwMS1hNzM3LTIzZTQ3MzYyNTE2MlwvZDQ5NDYyaC1jYWQxNDhiMi1jYzE1LTQxZGMtODhiZC1hMDEyMTcwNWFlNTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p3uszvKRYaQepDc1zptYWz8lNSBQtZZ3x_6rWas-wXY",
     name: "",
   },
   {
@@ -41,7 +41,7 @@ const data = [
   },
   {
     id: 5,
-    photo: "https://thumbs.gfycat.com/SlimSolidJapanesebeetle-max-1mb.gif",
+    photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dce29357-31f7-4a01-a737-23e473625162/d49462h-cad148b2-cc15-41dc-88bd-a0121705ae57.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZTI5MzU3LTMxZjctNGEwMS1hNzM3LTIzZTQ3MzYyNTE2MlwvZDQ5NDYyaC1jYWQxNDhiMi1jYzE1LTQxZGMtODhiZC1hMDEyMTcwNWFlNTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p3uszvKRYaQepDc1zptYWz8lNSBQtZZ3x_6rWas-wXY",
     name: "",
   },
   {
@@ -56,7 +56,7 @@ const data = [
   },
   {
     id: 8,
-    photo: "https://thumbs.gfycat.com/SlimSolidJapanesebeetle-max-1mb.gif",
+    photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dce29357-31f7-4a01-a737-23e473625162/d49462h-cad148b2-cc15-41dc-88bd-a0121705ae57.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZTI5MzU3LTMxZjctNGEwMS1hNzM3LTIzZTQ3MzYyNTE2MlwvZDQ5NDYyaC1jYWQxNDhiMi1jYzE1LTQxZGMtODhiZC1hMDEyMTcwNWFlNTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p3uszvKRYaQepDc1zptYWz8lNSBQtZZ3x_6rWas-wXY",
     name: "",
   },
   {
@@ -81,7 +81,7 @@ const data = [
   },
   {
     id: 13,
-    photo: "https://thumbs.gfycat.com/SlimSolidJapanesebeetle-max-1mb.gif",
+    photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dce29357-31f7-4a01-a737-23e473625162/d49462h-cad148b2-cc15-41dc-88bd-a0121705ae57.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZTI5MzU3LTMxZjctNGEwMS1hNzM3LTIzZTQ3MzYyNTE2MlwvZDQ5NDYyaC1jYWQxNDhiMi1jYzE1LTQxZGMtODhiZC1hMDEyMTcwNWFlNTcuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p3uszvKRYaQepDc1zptYWz8lNSBQtZZ3x_6rWas-wXY",
     name: "",
   },
   {
